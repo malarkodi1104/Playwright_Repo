0113f92cdba88b5db0a037ef86c7e050f1223949
malarkodi1104 <malarkodi1104@gmail.com>
2025-10-24 16:48:30 +0530

    Add files via upload
    Week1-Day2

---
+++ Week1_Day2_HomeWork/01-Assignment-Primitive-Datatypes.js
@@ -0,0 +1,33 @@
+/** Assignment Details:  
+Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`, 
+including an uninitialized variable. 
+ 
+Assignment Requirements:  
+    Create the following variables using let (not using var) and check their typeOf 
+    a) firstName 
+    b) companyName 
+    c) mobileNumber 
+    d) isAutomation 
+    e) hasPlaywright (do not assign) 
+ Print and confirm the values and data types */
+
+ let firstName = 'Malar';
+ console.log("Value of firstName is " +firstName);
+ console.log("Datatype of firstName is " +typeof(firstName));
+ 
+ let companyName = 'Testleaf';
+ console.log(`\nValue of companyName is ${companyName}`);
+ console.log(`Datatype of companyName is ${typeof(companyName)}`);
+ 
+ let mobileNumber = 7647289252;
+ console.log('\nValue of mobileNumber is '+mobileNumber);
+ console.log('Datatype of mobileNumber is '+typeof(mobileNumber));
+ 
+ let isAutomation = true;
+ console.log(`\nValue of isAutomation is ${isAutomation}`);
+ console.log(`Datatype of isAutomation is ${typeof(isAutomation)}`);
+
+ let hasPlaywright;
+ console.log("\nValue of hasPlaywright is "+hasPlaywright);
+ console.log("Datatype of hasPlaywright is "+typeof(hasPlaywright));
+   
